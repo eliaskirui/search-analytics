d7e5b60f983a1ffe990cd2801f0c0b6152a99445
Elias Kirui <eliaskips1@gmail.com>
2024-01-14 16:19:21 +0300

    Precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,5 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+Rake::Task["deploy:assets:precompile"].clear
